97f22b2581325d628f514f08e50011df6c5d6d19
Bryce Monaco <brycedmonaco@gmail.com>
2023-09-01 16:54:43 -0700

    Made Seek class a more generic steering class to have all behaviors in one, implemented flee with steering

--- Assets/_Scripts/Steering.cs
+++ Assets/_Scripts/Steering.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Implements math described in https://gamedevelopment.tutsplus.com/understanding-steering-behaviors-seek--gamedev-849t
-public class Seek : MonoBehaviour
+// Implements math described in https://gamedevelopment.tutsplus.com/series/understanding-steering-behaviors--gamedev-12732
+public class Steering : MonoBehaviour
 {
-    public enum SeekType
+    public enum SteeringType
     {
-        WithoutSteering,
-        WithSteering
+        SeekWithoutSteering,
+        SeekWithSteering,
+        FleeWithSteering
     }
-    [SerializeField] private SeekType seekType = SeekType.WithSteering;
+    [SerializeField] private SteeringType seekType = SteeringType.SeekWithSteering;
     [SerializeField] private Transform target;
     [SerializeField] private Rigidbody myRigidbody;
     [SerializeField] private float stopDistance = 5f;  // When we are this close or closer, stop
@@ -30,14 +31,17 @@ public class Seek : MonoBehaviour
     {
         switch (seekType)
         {
-            case SeekType.WithoutSteering:
+            case SteeringType.SeekWithoutSteering:
                 SeekWithoutSteering();
                 break;
-            case SeekType.WithSteering:
+            case SteeringType.SeekWithSteering:
                 SeekWithSteering();
                 break;
+            case SteeringType.FleeWithSteering:
+                FleeWithSteering();
+                break;
             default:
-                Debug.Log("Unhandled seek type");
+                Debug.LogError("Unhandled steering type");
                 break;
         }
     }
@@ -93,6 +97,50 @@ public class Seek : MonoBehaviour
         else
         {
             Vector3 desiredVelocity = (targetPos - myPos).normalized * maxVelocity;
+
+            /*
+             * This could be combined into one line, but breaking it up makes
+             * the math easier to follow.
+             */
+            steering = desiredVelocity - velocity;
+            steering = ClampVector(steering, maxVelocity);
+            steering = steering / myRigidbody.mass;
+            velocity = ClampVector(velocity + steering, maxVelocity);
+        }
+
+        if (drawDebugLines)
+        {
+            // Draw the desired direction vector
+            Debug.DrawLine(myPos, myPos + (velocity.normalized * debugLineLength), Color.magenta);
+            // Draw the current direction vector
+            Debug.DrawLine(myPos, myPos + (myRigidbody.velocity.normalized * debugLineLength), Color.green);
+
+        }
+
+        myRigidbody.velocity = velocity;
+    }
+
+    /**
+     * Seeking with steering behavior. When the target moves, this object will
+     * gradually change its direction over time, in this implementation, a
+     * higher mass for this object's rigidbody will mean it changes direction
+     * slower. Note that the object does not rotate to face its target, it just
+     * changes its velocity to move towards it.
+     */
+    void FleeWithSteering()
+    {
+        Vector3 myPos = transform.position;
+        Vector3 targetPos = target.position;
+        Vector3 velocity = myRigidbody.velocity;
+        Vector3 steering;
+
+        if (Vector3.Distance(myPos, targetPos) >= stopDistance)
+        {
+            velocity = Vector3.zero;  // We are far enough, stop
+        }
+        else
+        {
+            Vector3 desiredVelocity = (myPos - targetPos).normalized * maxVelocity;
 
             /*
              * This could be combined into one line, but breaking it up makes
